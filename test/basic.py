a2c30a7ae087596bac382219bf1389cad0cd9152
George King <george.w.king@gmail.com>
2016-08-22 13:53:48 -0400

    basic tests.

---
+++ test/basic.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from sys import argv
+
+
+def one_line(): return None #no-cov!
+
+def multi_line():
+  return None
+
+arg = int(argv[1])
+
+if arg:
+  one_line()
+  multi_line()
+  
